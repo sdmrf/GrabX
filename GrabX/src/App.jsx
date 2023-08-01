dce2ca88c1d35b69177b93b816b3b5efc01c17eb
Sdmrf <syedmarufali23@gmail.com>
2023-08-01 21:47:07 +0530

    Update

--- GrabX/src/App.jsx
+++ GrabX/src/App.jsx
@@ -1,6 +1,52 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import Product from "./Pages/Product/Product";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: (<Home />),
+      },
+      {
+        path: "/products/:id",
+        element: (<Products />),
+      },
+      {
+        path: "/product/:id",
+        element: (<Product />),
+      },
+    ]
+  }
+
+]);
+
+
 const App = () => {
   return (
-    <div className="App">Yo!</div>
+    <div> <RouterProvider router={router} /> </div>
   )
 }
 
