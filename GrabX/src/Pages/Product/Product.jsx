e2827c246aa3947a48746cc7134c9430b47b92c6
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-06 20:41:29 +0530

    Updated

--- GrabX/src/Pages/Product/Product.jsx
+++ GrabX/src/Pages/Product/Product.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import "./Product.scss"
+
+const images = [
+  "https://img.freepik.com/free-photo/fashion-portrait-young-elegant-woman_1328-2731.jpg",
+  "https://img.freepik.com/free-photo/fashion-portrait-young-elegant-woman_1328-2736.jpg",
+  "https://img.freepik.com/free-photo/fashion-portrait-young-elegant-woman_1328-2739.jpg"
+]
+
 const Product = () => {
+
+  const [currImg, setCurrImg] = useState(0);
+
   return (
-    <div>Product</div>
+    <div className="Product">
+      <div className="Left">
+      <div className="Images">
+        {images.map((img, index) => (
+          <img src={img} alt="" key={index} onClick={() => setCurrImg(index)} />
+        ))}
+      </div>
+      <div className="MainImg">
+        <img src={images[currImg]} alt="" />
+      </div>
+      </div>
+      <div className="Right">
+
+      </div>
+    </div>
   )
 }
 
