f2a4a6c8241d0fb63e9131ff60c80fc3f16a6aab
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-06 16:30:26 +0530

    Updated

--- GrabX/src/Pages/Products/Products.jsx
+++ GrabX/src/Pages/Products/Products.jsx
@@ -1,6 +1,52 @@
+import "./Products.scss"
+
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className="Products">
+      <div className="Left">
+        <div className="FilterItem">
+          <h1>Product Categories</h1>
+          <div className="Input">
+            <input type="checkbox" id="price1" name="price1" value="price1" />
+            <label htmlFor="price1">Shirt</label>
+          </div>
+          <div className="Input">
+            <input type="checkbox" id="price2" name="price2" value="price2" />
+            <label htmlFor="price2">Coat</label>
+          </div>
+        </div>
+        <div className="Input">
+          <input type="checkbox" id="price3" name="price3" value="price3" />
+          <label htmlFor="price3">Shoes</label>
+        </div>
+        <div className="FilterItem">
+          <h1>Filter By Price</h1>
+          <div className="Input">
+            <span>$0</span>
+            <input type = "range" min = {0} max = {1000} />
+            <span>$1000</span>
+          </div>
+
+        </div>
+        <div className="FilterItem">
+          <h1>Sort By</h1>
+          <div className="Input">
+            <input type="radio" id="asc" name="price" value="asc" />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+          <div className="Input">
+            <input type="radio" id="desc" name="price" value="desc" />
+            <label htmlFor="desc">Price (Highest First) </label>
+          </div>
+        </div>
+      </div>
+      <div className="Right">
+        <div className="Up">
+        <img src="" alt="" />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
