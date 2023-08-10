144aa683bdb62c36f1cc557d95853b325e4d56cb
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-10 11:47:10 +0530

    Updated

--- GrabX/src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ GrabX/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -4,10 +4,8 @@ import "./FeaturedProducts.scss";
 import axios from "axios";
 
 
-
-const GRABX_API_TOKEN = "a912ce73aa56ba47d9fd16146346c46fed79ea9fec7785ed4286a8b65e9651ef764303afdf053904bd4e4f1459293d1219bcf024e3dcd82ae084fb4ca6d26ca7ffc5ae16030794ddf367b20b83dc606e9c2ba13964044bd6ba2bf740f463742a2effb101763fe6ce9ef68ed21729435d7f328ddd9633642395f8b88cb637c121"
-const GRABX_API_URL = "http://localhost:1337/api"
-
+const URL = import.meta.env.VITE_APP_API_URL;
+const TOKEN = import.meta.env.VITE_APP_API_TOKEN;
 
 const FeaturedProducts = ({ type, info }) => {
 
@@ -16,8 +14,8 @@ const FeaturedProducts = ({ type, info }) => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await axios.get(GRABX_API_URL + `/products?populate=*&[filters][Type][$eq]=${type}`,{
-                    headers: { Authorization: `Bearer ${GRABX_API_TOKEN}` }
+                const response = await axios.get(URL + `/products?populate=*&[filters][Type][$eq]=${type}`,{
+                    headers: { Authorization: `Bearer ${TOKEN}` }
                 })
                 setProducts(response.data.data)
             } catch (error) {
